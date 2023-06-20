be93e836adbcd4241b931543887be7f68f137987
mpeng <michael.peng@bakkt.com>
2023-06-20 09:48:00 -0400

    first commit

---
+++ src/app/customer-detail/customer-detail.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
+import { Customer } from '../customer';
+
+@Component({
+  selector: 'app-customer-detail',
+  templateUrl: './customer-detail.component.html',
+  styleUrls: ['./customer-detail.component.scss']
+})
+export class CustomerDetailComponent {
+
+  @Input() customer:Customer = new Customer();
+  @Output() customerChange:EventEmitter<Customer> =new EventEmitter<Customer>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  update() {
+    this.customerChange.emit(this.customer);
+  }
+
+}
